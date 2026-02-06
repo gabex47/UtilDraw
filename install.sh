071431e778db03b103335061ce9686a46c5dafdc
Rian Jeph <rianjeph@Rians-MacBook-Pro.local>
2026-02-06 21:31:34 +0530

    Fix app name in installer

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ echo "Unzipping..."
 unzip -q "$TMP_DIR/$APP_NAME.zip" -d "$TMP_DIR"
 
 echo "Installing to /Applications..."
-cp -R "tmp/UtilDraw/UditDraw.app" /Applications/
+cp -R "tmp/UtilDraw/UtilDraw.app" /Applications/
 
 echo "Cleaning..."
 rm -rf "$TMP_DIR"
