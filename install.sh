eaa5cb4a6e792faf24b3275c62a57845d15a765a
Rian Jeph <rianjeph@Rians-MacBook-Pro.local>
2026-02-06 22:21:45 +0530

    Fix installer path for real

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 APP_NAME="UtilDraw"
@@ -9,20 +8,22 @@ ZIP_URL="https://github.com/gabex47/UtilDraw/releases/latest/download/UtilDraw.z
 echo "Installing $APP_NAME..."
 echo "Downloading..."
 
+# Clean temp
 rm -rf "$TMP_DIR"
 mkdir -p "$TMP_DIR"
 
+# Download zip
 curl -L "$ZIP_URL" -o "$TMP_DIR/$APP_NAME.zip"
 
 echo "Unzipping..."
 unzip -q "$TMP_DIR/$APP_NAME.zip" -d "$TMP_DIR"
 
 echo "Installing to /Applications..."
-TMP_DIR=" /tmp/UtilDraw.app"
-cp -R "$TMP_DIR/UtilDraw.app"  /Applications/
+sudo cp -R "$TMP_DIR/$APP_NAME.app" /Applications/
 
 echo "Cleaning..."
 rm -rf "$TMP_DIR"
 
 echo "Done! Launching $APP_NAME 🚀"
 open "/Applications/$APP_NAME.app"
+
